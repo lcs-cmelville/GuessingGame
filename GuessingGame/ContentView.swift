bb3221f35b2e59c69cc512b7624e954bf8dce497
Claire <cmelville@lcs.on.ca>
2021-12-02 09:21:18 -0500

    Got the basic layout for the game created, still static though.

--- GuessingGame/ContentView.swift
+++ GuessingGame/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            Slider(value: .constant(61.0),
+                   in: 0.0...100.0,
+                   step: 1.0,
+                   label: {
+                        Text("Guess")
+            },
+                   minimumValueLabel: {
+                        Text("0")
+            },
+                   maximumValueLabel: {
+                        Text("100")
+            })
+                .padding()
+            
+            Text("61")
+                .font(.largeTitle)
+                .bold()
+            
+            Button(action: {
+                print("Button was pressed")
+            }, label: {
+                Text("Submit Guess")
+            })
+            .buttonStyle(.bordered)
+            
+            Spacer()
+            
+        }
+        .navigationTitle("Guessing Game")
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        NavigationView{
         ContentView()
+        }
     }
 }
